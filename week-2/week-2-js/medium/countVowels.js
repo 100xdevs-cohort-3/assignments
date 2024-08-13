30283515e0488a6f5416180bc4d92acdc8fbc5e6
alsomedroo <dhruvattek@gmail.com>
2024-08-13 10:33:38 +0530

    countVowels

--- week-2/week-2-js/medium/countVowels.js
+++ week-2/week-2-js/medium/countVowels.js
@@ -7,6 +7,14 @@
 
 function countVowels(str) {
     // Your code here
+    let vow = 'aeiouAEIOU';
+    let c = 0;
+    for (let i = 0 ; i<str.length ; i++){
+      if (vowels.includes(str[i])) {
+        c++;
+      }
+    }
+    return c;
 }
 
 module.exports = countVowels;
