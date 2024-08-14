835c44f29427826123a1826d4af8201a75e220d4
ashik <ashiktm.ind@gmail.com>
2024-08-14 16:13:48 +0530

    exp and find largest completed

--- week-2/week-2-js/easy/expenditure-analysis.js
+++ week-2/week-2-js/easy/expenditure-analysis.js
@@ -13,8 +13,34 @@
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
-function calculateTotalSpentByCategory(transactions) {
-  return [];
+function calculateTotalSpentByCategory(transactions = []) {
+  let obj = new Map()
+  transactions.map((dat) => {
+    if (!obj.has(dat.category)) {
+      obj.set(dat.category, dat.price)
+    } else {
+      const price = obj.get(dat.category) + dat.price
+      obj.set(dat.category, price)
+    }
+  })
+  let res = []
+  obj.forEach((value, key) => {
+    res.push({ category: key, totalSpent: value })
+  })
+  console.log(res)
+  return res
+
 }
+
+let res = calculateTotalSpentByCategory( [
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  },
+])
+console.log(res);
 
 module.exports = calculateTotalSpentByCategory;
