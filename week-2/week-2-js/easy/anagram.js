22493b2c8012f6042c7387de0884981c59ca6f4f
Suleman Karigar <37236131+suleman1412@users.noreply.github.com>
2024-08-15 07:12:32 +0000

    solved anagram

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -5,7 +5,44 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  if (str1.length !== str2.length) {
+    return false;
+  }
 
+  if (str1.length === 0 && str2.length === 0) {
+    return true;
+  }
+
+  let str1Obj = {};
+  for (let i = 0; i < str1.length; i++) {
+    if (str1Obj[str1[i]]) {
+      //if letter in str1 aready exists in str1Obj ; increment
+      str1Obj[str1[i]]++;
+    } // add letter as a key and give a value of 1
+    else {
+      str1Obj[str1[i]] = 1;
+    }
+  }
+  for (let i = 0; i < str2.length; i++) {
+    //if letter in str2 in str1obj decrement value
+    if (str2[i] in str1Obj) {
+      str1Obj[str2[i]]--;
+    } else {
+      //if letter in str2 missing in str1obj return false
+      return false;
+    }
+  }
+
+  for (let key in str1Obj) {
+    if (str1Obj[key] !== 0) {
+      return false;
+    }
+  }
+  return true;
 }
+
+// console.log(isAnagram("Racecar", "racecar"));
 
 module.exports = isAnagram;
