a70d9ee8d7eaaf2b2faa48a192082937b28a58f7
mallsk <skollermallikarjun@gmail.com>
2024-08-15 10:48:20 +0530

    Anagram Solving

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -5,7 +5,28 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if(str1.length == str2.length)
+  {
+      const splited1 = str1.split("");
+      const splited2 = str2.split("");
+      console.log(splited1 , splited2);
+      const sorted1 = splited1.sort();
+      const sorted2 = splited2.sort();
+      console.log(sorted1,sorted2);
+      const joined1 = sorted1.join("");
+      const joined2 = sorted2.join("");
+      console.log(joined1,joined2);
+      if(joined1 == joined2)
+      {
+        console.log("Anagram");
+      }
+      else
+      {
+        console.log("It is not a Anagram");
+      }
+  }
+  else{
+    console.log("Error");
+  }
 }
-
 module.exports = isAnagram;
