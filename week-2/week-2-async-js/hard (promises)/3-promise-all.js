193dde92b8791fe1f1ffbb87181fea8b130086e2
Suleman Karigar <37236131+suleman1412@users.noreply.github.com>
2024-08-17 20:47:37 +0000

    promise all solved

--- week-2/week-2-async-js/hard (promises)/3-promise-all.js
+++ week-2/week-2-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,42 @@
  */
 
 function wait1(t) {
+  return new Promise((res) =>{
+    setTimeout(() =>{
+      res(`wait1 done`)
+    }, t * 1000)
+  })}
+                     
 
-}
 
 function wait2(t) {
-
-}
+return new Promise((res) =>{
+  setTimeout(() =>{
+    res(`wait2 done`)
+  }, t * 1000)
+})}
 
 function wait3(t) {
-
-}
+return new Promise((res) =>{
+  setTimeout(() =>{
+    res(`wait3 done`)
+  }, t * 1000)
+})}
 
 function calculateTime(t1, t2, t3) {
-
+  const rn = Date.now()
+  return Promise.all(
+      [
+        wait1(t1),
+        wait2(t2),
+        wait3(t3)
+      ]).then(() => {return Date.now() - rn})
 }
+
+// calculateTime(1,2,3).then(totalTime => {
+//   console.log(totalTime)
+//   console.log(typeof totalTime)
+// })
+
 
 module.exports = calculateTime;
