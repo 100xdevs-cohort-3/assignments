0c5dd2ee5ed8fce8919acec9b732c409f88e0fc1
karan gupta <gupta.karan8448@gmail.com>
2024-08-21 19:20:59 +0530

    solve 1-promisify-setTimeout.js

--- week-2/week-2-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-2/week-2-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,6 +3,13 @@
 */
 
 function wait(n) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, n*1000);
+    })
 }
+
+wait(2).then(() => {
+    console.log("Promise resolved, wait done.");
+})
 
 module.exports = wait;
