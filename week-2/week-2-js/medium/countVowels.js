984e1a73ab89acb4f66042ecf3c1f4c6097d6919
Dhanush D <draksharapudhanushsrisai.21ds@saividya.ac.in>
2024-09-09 22:24:08 +0530

    Week 2 Js Medium Assignments are completed

--- week-2/week-2-js/medium/countVowels.js
+++ week-2/week-2-js/medium/countVowels.js
@@ -5,8 +5,24 @@
   Once you've implemented the logic, test your code by running
 */
 
+function isVowel(letter){
+  vowels = ['a', 'e', 'i', 'o', 'u']
+
+  for(i = 0; i < vowels.length; i++){
+    if(letter === vowels[i]) return true;
+  }
+
+  return false;
+}
+
 function countVowels(str) {
     // Your code here
+    const strArr = str.split('');
+    
+    const result = strArr.filter((letter) => isVowel(letter));
+    return result.length;
 }
+
+console.log(countVowels('dhanush'));
 
 module.exports = countVowels;
--- week-2/week-2-js/medium/palindrome.js
+++ week-2/week-2-js/medium/palindrome.js
@@ -3,8 +3,29 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
+
 function isPalindrome(str) {
+  str = str.toLowerCase();
+
+  let low = 0;
+  let high = str.length-1;
+
+  while(low < high){
+    if(str[low] != str[high]){
+      return false;
+    }
+    low++;
+    high--;
+  }
+
   return true;
+}
+
+if(isPalindrome('Nitin')){
+  console.log("Given String is a palindrome");
+}
+else{
+  console.log("Given String is not a palindrome");  
 }
 
 module.exports = isPalindrome;
