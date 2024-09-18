d9ce2556cba6de2905c405350b82e871f028da29
Ritam acharya <ritamacharya2004@gmail.com>
2024-09-19 00:12:11 +0530

    6.1-todo backend assignment completed

--- week-6/6.1-todo/backend/index.js
+++ week-6/6.1-todo/backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { getAllTodo, createTodo, updateTodo, deleteTodoById } = require('./routes/todo'); // importing callback functions for routes
+const { getAllTodo, createTodo, updateTodo, deleteTodoById, deleteTodo, searchTodo } = require('./routes/todo'); // importing callback functions for routes
 const app = express();
 const PORT = 3001;
 
@@ -20,8 +20,13 @@ app.put('/todos/:id', updateTodo);
 // Delete a todo
 app.delete('/todos/:id', deleteTodoById);
 
+// Delete all todos
+app.delete("/todos" , deleteTodo);
+
 
 // TODO: can you implement search todo route ???
+
+app.get("/todo" , searchTodo);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- week-6/6.1-todo/backend/routes/todo.js
+++ week-6/6.1-todo/backend/routes/todo.js
@@ -1,21 +1,131 @@
 let todos = []; // in memory space
+let count = 0;
+let deletedTodos = [];
 
-export async function getAllTodo (req, res, next){
+async function getAllTodo (req, res, next){          // to get all your todos history
     //  write here
+    res.json({
+        todos,
+        deletedTodos
+    });
+    
 }
 
-export async function createTodo (req, res, next){
+async function createTodo (req, res, next){
     //  write here
+    const todo = req.body.todo;
+    count++;
+    if(todo){
+        todos.push({
+            id : count,
+            todo : todo
+        });
+        res.json({
+            message : "todo create successfully...."
+        })
+    }else {
+        res.status(400).json({
+            message : "unable to get the todo..."
+        });
+    }
+    
 }
 
-export async function updateTodo (req, res, next){
+async function updateTodo (req, res, next){
     //  write here
+    const id = parseInt(req.params.id);
+    const updatedTodo = req.body.todo;
+    if (id && updatedTodo){
+        let foundTodo = null;
+        for (let i=0; i<todos.length; i++){
+            if (todos[i].id === id){
+                foundTodo = todos[i];
+                todos[i].todo = updatedTodo;
+            }
+        }
+        if (foundTodo){
+            res.json({
+                message : "updated successfully...."
+            })
+        }else {
+            res.json({
+                message : "can't get your previous data...."
+            })
+        }
+    }else {
+        res.json({
+            message : "Something went wrong while receiving your data..."
+        })
+    }
+
 }
 
-export async function deleteTodo (req, res, next){
+async function deleteTodo (req, res, next){      // to delete all your todos history
     //  write here
+    todos.splice(0,todos.length);
+    deletedTodos.splice(0 , deletedTodos.length);
+    res.json({
+        message : "you have deleted all the todos..."
+    })
 }
 
-export async function deleteTodoById (req, res, next){
+async function deleteTodoById (req, res, next){
     //  write here
+    const id = parseInt(req.params.id);
+    if (id){
+        for (let i=0; i<todos.length; i++){
+            if(todos[i].id === id){
+                const arr = todos.splice(i,1);
+                deletedTodos.push(arr[0]);
+            }
+        }
+        res.json({
+            message : "delete successfully..."
+        });
+    }else {
+        res.json({
+            message : "unable to get the id..."
+        });
+    }
+    
 }
+
+
+
+
+async function searchTodo (req , res , next){
+    const todo = req.body.todo;
+    if (todo){
+        let getTodo = null;
+        for (let i=0; i<todos.length; i++){
+            if(todos[i].todo === todo){
+                getTodo = todos[i];
+                break;
+            }
+        }
+        if(getTodo){
+            res.json({
+                searchedTodo : getTodo
+            })
+        }else {
+            res.json ({
+                message : "can't get your todo..."
+            });
+        }
+    }else {
+        res.json ({
+            message : "Something went wrong while receiving your data..."
+        });
+    }
+}
+
+
+
+module.exports = {
+    deleteTodoById ,
+    deleteTodo ,
+    updateTodo ,
+    createTodo ,
+    getAllTodo ,
+    searchTodo
+};
