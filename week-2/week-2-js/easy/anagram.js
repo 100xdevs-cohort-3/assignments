deafc3cbf285c53559d99570a9eecd3d4e96e5ae
LOVISH GOYAL <112848018+LOVISH066@users.noreply.github.com>
2024-09-21 00:41:12 +0530

    Update anagram.js

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -6,8 +6,21 @@
 
 function isAnagram(str1, str2) 
 {
-  let sortedstr1 = str1.sort(); // abccba -> aabbcc
-  let sortedstr2 = str2.sort();
+//let arr1 = [];
+//arr1 = [...str1]; // or use slpit function
+let strg1 = str1.toLowerCase();
+let strg2 = str2.toLowerCase();
+let a = strg1.split("");
+let b = strg2.split("");
+//let arr2 = [];
+//arr2 = [...str2];
+  let c = a.sort(); // abccba -> aabbcc
+  let d = b.sort(); // sort() funcvtion can only e use with array
+// here we will use join function join()
+let sortedstr1 = c.join();
+let sortedstr2 = d.join();
+
+
   if(sortedstr1 == sortedstr2) {
     return true;
   }else
