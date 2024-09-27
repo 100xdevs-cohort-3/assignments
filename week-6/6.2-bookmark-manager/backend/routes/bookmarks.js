145bce26c48e3f359a4043fc72fa3d999fcfa970
Manoj <manojbhichchhar8@gmail.com>
2024-09-27 19:10:33 +0530

    bookmark-backend

--- week-6/6.2-bookmark-manager/backend/routes/bookmarks.js
+++ week-6/6.2-bookmark-manager/backend/routes/bookmarks.js
@@ -1,13 +1,42 @@
 let bookmarks = []; // in memory space
 
-export async function addBookmark(req,res,next){
+async function addBookmark(req,res,next){
 // write here
+    //validate the details
+    const {url,category}=req.body;
+    if(!url||!category){
+        throw new Error("Details required")
+    }
+
+    //creating a bookmark
+    const bookmark={
+        url:url,
+        category:category
+    }
+    //saving the bookmark
+    bookmarks.push(bookmark);
+
+    return res.status(201).send({message:"Bookmark created successfully"})
 }
 
-export async function deleteBookmark(req,res,next){
+async function deleteBookmark(req,res,next){
 // write here
+    //validate the details
+    const id=req.params.id;
+    if(id===''){
+        throw new Error("Detais required")
+    }
+    //delete the bookmark
+    bookmarks.splice(id,1);
+
+    return res.status(200).send({message:"Boookmark deleted succesfully"})
+
 }
 
-export async function getAllBookmarks(req,res,next){
+async function getAllBookmarks(req,res,next){
 // write here
+    return res.status(200).send(bookmarks)
 }
+
+
+module.exports={addBookmark,deleteBookmark,getAllBookmarks}
