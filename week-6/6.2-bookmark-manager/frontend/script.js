e8bfa76b893b06bd820ce0a09cbea4c2607db8db
Manoj Kumar <manoj8@Manojs-MacBook-Air.local>
2024-09-27 23:56:24 +0530

    vf

--- week-6/6.2-bookmark-manager/frontend/script.js
+++ week-6/6.2-bookmark-manager/frontend/script.js
@@ -3,11 +3,25 @@ const API_URL = 'http://localhost:3001/bookmarks';
 // Fetch bookmarks when the page loads
 document.addEventListener('DOMContentLoaded', () => {
 //   start here
+    fetchBookmarks();
 });
 
 // Fetch bookmarks from the backend
 function fetchBookmarks() {
     //  start here
+    fetch(`${API_URL}`)
+    .then(res=>res.json())
+    .then(data=>{
+        const bookmarkList=document.getElementById('bookmark-list');
+        data.foreach(bookmark=>{
+            console.log(bookmark);
+            const divEl=document.createElement('div');
+            divEl.innerHTML=`${bookmark.url}+(+${bookmark.category}+)`;
+            bookmarkList.appendChild(divEl);
+            
+            
+        })
+    })
 }
 
 // Add a bookmark to the DOM
