eedc459003e80d05107b6c194044b58e1b2fdb65
Vikash Thakur <thakvikash622@gmail.com>
2024-09-28 18:20:48 +0530

    SetUp: Routing using reacr-router-dom

--- week-7/client/src/App.jsx
+++ week-7/client/src/App.jsx
@@ -1,11 +1,19 @@
 // firstly, Don't get overwhelmed and if you are then go with client-easy.
+import { BrowserRouter, Route, Routes} from "react-router-dom"
 import Home from "./pages/Home"
+import Register from "./components/Register"
+import Login from "./components/Login"
 function App() {
 
   return (
     <>
-      {/* start  writing from here */}
-       <Home/>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/adminsignup" element={<Register />} />
+      <Route path="/adminlogin" element={<Login />} />
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- week-7/client/src/pages/Home.jsx
+++ week-7/client/src/pages/Home.jsx
@@ -1,16 +1,21 @@
-// compoents imports
-// import Login from '../components/Login'
-// import Register from '../components/Register'
-import Courses from '../components/Courses'
+import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
     //  write home page UI code here
     <div>
-      <h1>Home</h1>
-      {/* <Register />
-      <Login /> */}
-      <Courses />
+      <h1>Welcome to Coursifiy</h1>
+      <div>
+        <Link to={"/adminlogin"}>
+          <div>
+            <h1>Admin</h1>
+          </div>
+        </Link>
+        {/* will work on this later */}
+        {/* <div>
+          <h1>User</h1>
+        </div> */}
+      </div>
     </div>
   )
 }
