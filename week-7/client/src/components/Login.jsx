052acda7a9017f7691e68be331b52ae54cdb2e51
Vikash Thakur <thakvikash622@gmail.com>
2024-09-28 23:49:36 +0530

    Updated login page

--- week-7/client/src/components/Login.jsx
+++ week-7/client/src/components/Login.jsx
@@ -1,19 +1,22 @@
 // login code here
 import axios from 'axios';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+    const navigate = useNavigate()
 
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
 
     // call the functions onClick of button.
-    async function handleLogin() {
+    const handleLogin = async () => {
         try {
-            const resposne = await axios.post("http://localhost:3000/admin/login", {username, password}); 
-            alert(resposne.data.message)
-            console.log(resposne.data);
+            const resposne = await axios.post("http://localhost:3000/admin/login", { username, password });
             localStorage.setItem("adminToken", resposne.data.token)
+            console.log(resposne.data.message);
+            navigate("/admindashboard")
         } catch (error) {
             console.log("Error", error);
         }
@@ -21,9 +24,9 @@ const Login = () => {
     return (
         <div>
             <h1>Login</h1>
-            <input type="text" placeholder='UserName' value={username} onChange={(e) => setUsername(e.target.value)} />
-            <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={handleLogin}>Submit</button>
+                <input type="text" placeholder='UserName' value={username} onChange={(e) => setUsername(e.target.value)} />
+                <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button onClick={handleLogin}>Login</button>
         </div>
     )
 }
