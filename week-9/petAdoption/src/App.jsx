8f24e65c9065d2f5843deedcd93ae865ce716e47
Akshat Sipany <borncancer21@gmail.com>
2024-10-12 16:57:08 +0530

    formData State being set and received

--- week-9/petAdoption/src/App.jsx
+++ week-9/petAdoption/src/App.jsx
@@ -1,14 +1,20 @@
 import "./App.css";
 import Header from "./components/Header";
 import PetAdoptionForm from "./components/PetAdoptionForm";
+import { useState } from "react";
 
 function App() {
+  const [formData, setFormData] = useState([]);
   return (
     <>
       <Header></Header>
       <div className="app-container">
-        <PetAdoptionForm></PetAdoptionForm>
+        <PetAdoptionForm
+          formData={formData}
+          setFormData={setFormData}
+        ></PetAdoptionForm>
       </div>
+      {console.log(formData)}
     </>
   );
 }
--- week-9/petAdoption/src/components/PetAdoptionForm.jsx
+++ week-9/petAdoption/src/components/PetAdoptionForm.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-
-const PetAdoptionForm = () => {
+import React from "react";
+const PetAdoptionForm = ({ formData,setFormData }) => {
   const containerStyle = {
     border: "2px solid black",
     margin: "auto",
-    // height: "60vh",
     width: "50vh",
     padding: "10px",
     display: "flex",
@@ -26,78 +24,100 @@ const PetAdoptionForm = () => {
     fontWeight: "bold",
     fontSize: 24,
   };
+  const handleSumbit = ()=>{
+    setFormData([...formData,{
+      name : document.getElementById('pet-name').value,
+      type : document.getElementById('pet-type').value,
+      breed : document.getElementById('pet-breed').value,
+      userName : document.getElementById('user-name').value,
+      userEmail : document.getElementById('user-email').value,
+      userPhone : document.getElementById('user-phone').value
+    }])
+    alert('Data has been recorded.Thank You.')
+    document.getElementById('pet-name').value = ''
+    document.getElementById('pet-type').value = ''
+    document.getElementById('pet-breed').value = ''
+    document.getElementById('user-name').value = ''
+    document.getElementById('user-email').value = ''
+    document.getElementById('user-phone').value = ''
+  }
   return (
-    <div className='container' style={containerStyle}>
+    <div className="container" style={containerStyle}>
       <div className="pet-name" style={inputDiv}>
-          <div style={inputLabel}>Pet Name</div>
-          <input
-            type="text"
-            name=""
-            id=""
-            style={inputStyle}
-            placeholder={"Pet Name"}
-          />
-        </div>
-        <div className="pet-type" style={inputDiv}>
-          <div style={inputLabel}>Pet Type</div>
-          <input
-            type="text"
-            name=""
-            id=""
-            style={inputStyle}
-            placeholder={"Pet Type"}
-          />
-        </div>
-        <div className="pet-breed" style={inputDiv}>
-          <div style={inputLabel}>Breed</div>
-          <input
-            type="text"
-            name=""
-            id=""
-            style={inputStyle}
-            placeholder={"Breed"}
-          />
-        </div>
-        <div className="user-name" style={inputDiv}>
-          <div style={inputLabel}>Your Name</div>
-          <input
-            type="text"
-            name=""
-            id=""
-            style={inputStyle}
-            placeholder={"Name"}
-          />
-        </div>
-        <div className="user-email" style={inputDiv}>
-          <div style={inputLabel}>Email</div>
-          <input
-            type="email"
-            name=""
-            id=""
-            style={inputStyle}
-            placeholder={"Email"}
-          />
-        </div>
-        <div className="user-phone" style={inputDiv}>
-          <div style={inputLabel}>Phone</div>
-          <input
-            type="text"
-            name=""
-            id=""
-            style={inputStyle}
-            placeholder={"Phone"}
-          />
-        </div>
-        <div className="submit">
-          <button style={{
+        <div style={inputLabel}>Pet Name</div>
+        <input
+          type="text"
+          name=""
+          id="pet-name"
+          style={inputStyle}
+          placeholder={"Pet Name"}
+        />
+      </div>
+      <div className="pet-type" style={inputDiv}>
+        <div style={inputLabel}>Pet Type</div>
+        <input
+          type="text"
+          name=""
+          id="pet-type"
+          style={inputStyle}
+          placeholder={"Pet Type"}
+        />
+      </div>
+      <div className="pet-breed" style={inputDiv}>
+        <div style={inputLabel}>Breed</div>
+        <input
+          type="text"
+          name=""
+          id="pet-breed"
+          style={inputStyle}
+          placeholder={"Breed"}
+        />
+      </div>
+      <div className="user-name" style={inputDiv}>
+        <div style={inputLabel}>Your Name</div>
+        <input
+          type="text"
+          name=""
+          id="user-name"
+          style={inputStyle}
+          placeholder={"Name"}
+        />
+      </div>
+      <div className="user-email" style={inputDiv}>
+        <div style={inputLabel}>Email</div>
+        <input
+          type="email"
+          name=""
+          id="user-email"
+          style={inputStyle}
+          placeholder={"Email"}
+        />
+      </div>
+      <div className="user-phone" style={inputDiv}>
+        <div style={inputLabel}>Phone</div>
+        <input
+          type="text"
+          name=""
+          id="user-phone"
+          style={inputStyle}
+          placeholder={"Phone"}
+        />
+      </div>
+      <div className="submit">
+        <button
+          style={{
             width: "20vw",
-            padding : '10px',
-            fontWeight : 'bold',
-            fontSize : "16px"
-            }}>Submit</button>
-        </div>
+            padding: "10px",
+            fontWeight: "bold",
+            fontSize: "16px",
+          }}
+          onClick={handleSumbit}
+        >
+          Submit
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PetAdoptionForm
+export default PetAdoptionForm;
