a2b967caec85678189bf1ab286668fb454204e61
Akshat Sipany <borncancer21@gmail.com>
2024-10-12 17:35:03 +0530

    tableData being retrieved and populated

--- week-9/petAdoption/src/App.jsx
+++ week-9/petAdoption/src/App.jsx
@@ -2,17 +2,32 @@ import "./App.css";
 import Header from "./components/Header";
 import PetAdoptionForm from "./components/PetAdoptionForm";
 import { useState } from "react";
+import TableData from "./components/TableData";
 
 function App() {
   const [formData, setFormData] = useState([]);
+  const [tableView, setTableView] = useState(false)
+  const buttonStyle = {
+    width: "20vw",
+    padding: "8px",
+    fontWeight: "bold",
+    fontSize: "16px",
+    position : "fixed",
+    right : 0
+  };
   return (
     <>
       <Header></Header>
+      <button style={buttonStyle} onClick={()=>{
+        setTableView(tableView=>!tableView)
+      }}>See Data</button>
       <div className="app-container">
         <PetAdoptionForm
           formData={formData}
           setFormData={setFormData}
         ></PetAdoptionForm>
+        {/* {tableView?<TableData formData={formData}></TableData>:alert('No data')} */}
+        
       </div>
       {console.log(formData)}
     </>
--- week-9/petAdoption/src/components/TableData.jsx
+++ week-9/petAdoption/src/components/TableData.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
 
-const TableData = () => {
+const TableData = ({ formData }) => {
+  const tableStyle = {
+    width: "80%",
+    margin: "50px auto",
+    borderCollapse: "collapse",
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+    color: "white",
+  };
+  const thStyle = {
+    backgroundColor: "#b9a16f",
+    color: "darkgreen",
+    fontWeight: "bold",
+    padding: "15px",
+    textAlign: "center",
+    border: "1px solid green",
+    backgroundColor: "#c4a484",
+    fontSize : 22
+  };
+  const tdStyle = {
+    color: "darkgreen",
+    padding: "15px",
+    textAlign: "center",
+    border: "1px solid green",
+    backgroundColor: "#c4a484",
+    fontSize : 18
+  };
   return (
-    <div>TableData</div>
-  )
-}
+    <div className="container">
+      <table style={tableStyle}>
+        <thead>
+          <tr>
+            <th style={thStyle}>Pet Name</th>
+            <th style={thStyle}>Pet Type</th>
+            <th style={thStyle}>Breed</th>
+            <th style={thStyle}>Adopter Name</th>
+            <th style={thStyle}>Email</th>
+            <th style={thStyle}>Phone </th>
+          </tr>
+        </thead>
+        <tbody>
+          {formData.map((data) => {
+            return (
+              <tr>
+                <td style={tdStyle}>{data.name}</td>
+                <td style={tdStyle}>{data.type}</td>
+                <td style={tdStyle}>{data.breed}</td>
+                <td style={tdStyle}>{data.userName}</td>
+                <td style={tdStyle}>{data.userEmail}</td>
+                <td style={tdStyle}>{data.userPhone}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default TableData
+export default TableData;
