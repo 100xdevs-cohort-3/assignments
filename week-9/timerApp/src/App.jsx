6a0c42ca7820f69ea4b31567bd0ea897fba139a5
Akshat Sipany <borncancer21@gmail.com>
2024-10-19 23:41:22 +0530

    init

--- week-9/timerApp/src/App.jsx
+++ week-9/timerApp/src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+function App() { 
+  const [time, setTime] = useState(100)
+  const containerStyle = {
+    
+  }
   return (
-      <>
-        
-      </>
-  )
+    <>
+      <div className="container" style={containerStyle}>
+        <div className="timeHolder">
+          
+        </div>
+        <div className="options">
+          <button>Pause</button>
+          <button>Reset</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
