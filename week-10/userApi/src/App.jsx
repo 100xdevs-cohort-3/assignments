768dc53cd67ce7dbcab2d283c2b74cd079262b06
Anshuman Laskar <anshumanlaskar1@gmail.com>
2024-10-21 09:20:45 +0530

    pagination added !

--- week-10/userApi/src/App.jsx
+++ week-10/userApi/src/App.jsx
@@ -1,57 +1,61 @@
-  import { useState, useEffect } from "react";
-  import RandomUser from "./components/RandomUser";
+import { useState, useEffect } from "react";
 
-  function App() {
-    const [users, setUsers] = useState([]);
+import UserCollection from "./components/UserCollection" 
+import Pagination from "./components/Pagination";
+function App() {
+  const [users, setUsers] = useState([]);
+  const [usersPerPage, setUsersPerPage] = useState(30);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [loading,setLoading] = useState(false);
+  //range of one page : [(currentPage-1)*usersPerPage, currentPage*usersPerPage]
+  async function getUser() {
+    let response = await fetch("https://randomuser.me/api/");
+    response = await response.json();
 
-    async function getUser(){
-      let response = await fetch("https://randomuser.me/api/");
-      response = await response.json();
-
-      console.log(response);
-      return response;
-    }
-
-
-    async function addUser() {
-      let response = await getUser();
-
-      let name = `${response.results[0].name.first} ${response.results[0].name.last}`
-      let image = response.results[0].picture.large;
-      
-      setUsers((prev) => [
-        ...prev,
-        {
-          userImage: image,
-          userName: name,
-        },
-      ]);
-    }
-
-    useEffect(() => {
-      addUser();
-    }, []);
-
-    return (
-      <div style={{textAlign:"center"}}>
-        <h1>Random Users</h1>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "Center",
-            border: "5px solid black",
-            flexWrap: "wrap",
-          }}
-        >
-          {users.map((user, index) => {
-            return (
-              <RandomUser key={index} userImage={user.userImage} userName={user.userName} />
-            );
-          })}
-        </div>
-        <button onClick={addUser}>Add</button>
-      </div>
-    );
+    console.log(response);
+    return response;
   }
 
-  export default App;
+  async function addUsers() {
+    setLoading(true);
+    let newUsers=[]
+    for(let i=1;i<=10;i++){
+      let response = await getUser();
+  
+      let name = `${response.results[0].name.first} ${response.results[0].name.last}`;
+      let image = response.results[0].picture.large;
+      newUsers.push({userImage: image, userName:name})
+    }
+
+    setUsers((prev) => [
+      ...prev,
+      ...newUsers,
+    ]);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    addUsers();
+  }, []);
+
+  function nextPage(){
+    setCurrentPage(prev => prev+1);
+  }
+  function prevPage(){
+    setCurrentPage(prev => prev-1);
+  }
+  const usersToShow =  users.slice(((currentPage-1)*usersPerPage), (currentPage*usersPerPage));
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Random Users</h1>
+      <UserCollection usersToShow={usersToShow} loading={loading}/>
+      {currentPage > 1 && <button onClick={prevPage}>Prev Page</button>}
+      <button onClick={addUsers}>Add</button>
+      {users.length > usersPerPage && <button onClick={nextPage}>NextPage</button>}
+
+      <Pagination totalUsers={users.length} usersPerPage={usersPerPage} setCurrentPage={setCurrentPage}/>
+    </div>
+  );
+}
+
+export default App;
