de2c5d73a218dc7b7e10e4a867000e82f64e3704
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2024-10-23 23:38:27 +0530

    updated

--- week-5/middleWare.js
+++ week-5/middleWare.js
@@ -2,12 +2,17 @@ const express=require("express");
 
 const app=express();
 
-let requestIncreaser=0;
+let requestCount=0;
 
 function requestIncreaser(req,res,next){
     requestCount = requestCount + 1;
     req.name="randonepuja2234";
     console.log("Total number of reqquests = " + requestCount);
+    //next();
+    res.json({
+        msg: "I ended the request early",
+    })
+    next();
 
 }
 
@@ -22,7 +27,9 @@ function realSumHandler(req,res){
 }
 
 
-//middleware
+//global middleware
+
+//middleware  
 app.get("/sum",requestIncreaser,realSumHandler);
 
 app.listen(3000);
