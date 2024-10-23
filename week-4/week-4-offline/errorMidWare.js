99985995507c6cb982bc2159db1906705a833358
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2024-10-23 20:23:58 +0530

    update week-5 files

--- week-4/week-4-offline/errorMidWare.js
+++ week-4/week-4-offline/errorMidWare.js
@@ -9,8 +9,23 @@ let errorCount=0;
 // 2. maintain the errorcount variable whose value shold go up every time there is an expection in any end point
 
 app.get("/user",function(req,res){
-    throw new Error("User not found");
+    throw new Error("Some error");
     res.status(200).json({name: 'json'});
 });
  
+
+app.post("/user",function(req,res){
+    res.status(200).json({name: 'created dummy user'});
+});
+
+app.get("/errorCount",function(req,res){
+    res.status(200).json({errorCount});
+});
+
+//error handling middleware at the end we defind this
+app.use(function(err,req,res,next){
+    res.status(404).send({})
+    errorCount =errorCount +1;
+})
+
 app.listen(3000);
