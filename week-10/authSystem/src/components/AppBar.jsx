2d60415386a5306fb03dafe907c775b202503dc6
Anshuman Laskar <anshumanlaskar1@gmail.com>
2024-10-29 21:24:59 +0530

    new init

--- week-10/authSystem/src/components/AppBar.jsx
+++ week-10/authSystem/src/components/AppBar.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Login from './Login'
 
 const AppBar = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+  
+  function login(username) {
+    setLoggedIn(true);
+    setUsername(username);
+  }
+  function logout(){
+    setLoggedIn(false);
+    setUsername("");
+  }
   return (
-    <div>AppBar</div>
+    <div style={{ backgroundColor : "rgb(64, 152, 181)", height:"100px", display:"flex",padding:10}}>
+    <div>
+        <h1 style={{color:"white"}}>Auth System</h1>
+    </div>
+        {loggedIn ? <div style={{marginLeft:"auto", fontSize:"30px", color:"white", display:"flex"}}>Welcome {username} <button onClick={logout} style={{height:"30px", marginLeft:10}}>Logout</button></div> : <Login login={login}/>}
+    </div>
   )
 }
 
