7ec8180b82586148c57135c9fa9440ae997e09ca
Anshuman Laskar <anshumanlaskar1@gmail.com>
2024-10-30 01:54:35 +0530

    routing added !

--- week-11/amazonCart/src/components/AppBar.jsx
+++ week-11/amazonCart/src/components/AppBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import CartIcon from './CartIcon'
-export default function AppBar(){
+import WishListIcon from './WishListIcon'
+export const AppBar = React.memo( function AppBar(){
   return (
     <div style={{display:"flex",color:"white",backgroundColor:"rgb(16, 24, 39)", height:"15%",width:"100%", alignItems:"center"}}>
       <div style={{ fontSize:"3.5rem" ,marginLeft:10}}>
@@ -9,19 +9,10 @@ export default function AppBar(){
       </div>
       <CartIcon/>
       <WishListIcon/>
+      <WishListIcon/>
     </div>
   )
-}
-
-function WishListIcon(){
-  return (
-    <Link to="/wishlist">
-    <div>
-      Wish
-    </div>
-    </Link>
-  )
-}
+})
 
 
 
