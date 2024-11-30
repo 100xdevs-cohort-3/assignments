f97bfeb308b1750d6761ac92d1f81dab232469d1
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2024-11-30 13:49:18 +0530

    commit

--- week-10/my_file/domfocus3.jsx
+++ week-10/my_file/domfocus3.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-import {useState,useRef} from "react";
+
 
 function App(){
-  const inputRef=useRef();
+
 
 
   function focusOnInput(){
-    //document.getElementById("name").focus();
-    inputRef.current.focus();
+    document.getElementById("name").focus();
+
   }
   return <div>
     Sign up
-    <input ref={inputRef} id="name" type={"text"} ></input>
+    <input id="name" type={"text"} ></input>
     <input type="text" />
     <button onClick={focusOnInput}>submit</button>
 
