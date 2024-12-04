868e165d84225d6db52d63cb2d008703932c8c80
sakshamgiri2023 <sakshamgiri500@gmail.com>
2024-12-04 23:07:28 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import { useState, useEffect, } from 'react'
+import Timer from './components/Timer'
+
+function App() {
+
+  return (
+    <>
+      <Timer />
+    </>
+  )
+}
+
+export default App
