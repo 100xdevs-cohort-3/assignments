cf41889f3edb86aa2e3f20356b5b58b360154ea0
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2024-12-08 21:39:51 +0530

    advanced recoil updated

---
+++ week-11-offline/recoil_advance/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+ return (
+  <div>
+    <button>Home</button>
+    <button>My network ()</button>
+    <button>Messaging ()</button>
+    <button>Job ()</button>
+    <button>Notifications ()</button>
+    <button>Me</button>
+  </div>
+ )
+}
+
+export default App
