83a2221995dd7dd0dd722207bc6a0fcd62226d30
HarshDevs10 <harshdevsinghthakur8@gmail.com>
2025-01-01 17:26:22 +0000

    week-4 hard userAuth

--- week-4/hard/database/index.js
+++ week-4/hard/database/index.js
@@ -1,16 +1,25 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect(process.env.MongoDB_Url);
 
 // Define schemas
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    email: {type: String},
+    password: {type: String},
+    firstName: {type: String},
+    lastName: {type: String}
 });
 
 const TodoSchema = new mongoose.Schema({
     // Schema definition here
+    title: {type: String},
+    description: {type: String},
+    done: {type: Boolean},
+    userId: {type: mongoose.Types.ObjectId, ref: 'User'}
 });
 
 const User = mongoose.model('User', UserSchema);
--- week-4/hard/middleware/user.js
+++ week-4/hard/middleware/user.js
@@ -1,5 +1,30 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+
 function userMiddleware(req, res, next) {
     // Implement user auth logic
+    const token = req.headers.token
+
+    if (!token){
+        return res.json({
+            mes: "please enter your valid jwt token"
+        })
+    }
+
+    try{
+        const decoded = jwt.verify(token,process.env.User_Password)
+        if (decoded){
+            req.userId = decoded.id
+            next()
+        }
+    }
+    catch(err){
+        return res.json({
+            mes: "an error occured while verifying the token",
+            err: err
+        })
+    }
+
 }
 
 module.exports = userMiddleware;
