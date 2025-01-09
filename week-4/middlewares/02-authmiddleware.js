31bf7b0a86e48c9fdfc98cbd5f1b066be4fe6da8
Devansh Bhavsar <devanshbhavsar2006@gmail.com>
2025-01-09 15:56:26 +0530

    solved 02-authmiddleware

--- week-4/middlewares/02-authmiddleware.js
+++ week-4/middlewares/02-authmiddleware.js
@@ -1,22 +1,24 @@
-
 //  Implement an authentication middleware that checks for a valid API key in the request headers.
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const VALID_API_KEY = '100xdevs_cohort3_super_secret_valid_api_key'; // key is 100xdevs-api-key use that API key for authenticate user
-
+const VALID_API_KEY = "100xdevs_cohort3_super_secret_valid_api_key"; // key is 100xdevs-api-key use that API key for authenticate user
 
 // Middleware to check for a valid API key
 function authenticateAPIKey(req, res, next) {
-    //  authenticate APIKey here
+  const apiKey = req.headers["100xdevs-api-key"];
+
+  if (apiKey !== VALID_API_KEY) {
+    return res.status(401).json({ message: "Invalid or missing API key" });
+  } else {
+    next();
+  }
 }
 
 app.use(authenticateAPIKey);
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Access granted' });
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Access granted" });
 });
 
 module.exports = app;
-
-
