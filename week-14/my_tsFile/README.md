2c1ae2526fb7ed74943418a2d8ab020c3c6074b4
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2025-01-26 18:31:12 +0530

    commit typescript files

---
+++ week-14/my_tsFile/README.md
@@ -0,0 +1,26 @@
+## start typecript project
+
+````bash````
+npm init -y
+
+````
+
+````bash````
+
+npm install -g typescript 
+````
+
+````bash````
+tsc
+````
+
+
+````bash````
+
+npx tsc -b
+
+``````
+
+````bash````
+node index.js
+````
---
+++ week-14/my_tsFile/index.ts
@@ -0,0 +1,64 @@
+// let x: number=1;
+// console.log(x);
+
+
+
+
+// function greet( firstName: string){
+//     console.log("Good morning " + firstName);
+// }
+
+
+// greet("Puja");
+
+
+
+// function sum (firstNo: number, secondNo : number){
+//     console.log(firstNo + secondNo);
+// }
+
+
+// sum(5,7);
+
+ // sum
+// function sum (a:number,b:number){
+//     return a+b;
+// }
+
+// let x=sum(7,8);
+// console.log(x);
+
+
+//legal age
+// function legalAge(age: number){
+//     if(age >18)
+//        return true;
+//     else 
+//         return false;
+// }
+
+// console.log(legalAge(20));
+
+
+//4) write a function that takes anothre function as an input and runs after 1 second
+
+// function delayedCall(fn:()=>void){
+//     setTimeout(fn,1000);
+// }
+
+// delayedCall(function(){
+//     console.log("Hello")
+// })
+
+
+//other
+function delayedCall(anotherFn:()=>void){
+    setTimeout(anotherFn,1000);
+}
+
+function log(){
+    console.log("Hii there");
+
+}
+
+delayedCall(log)
