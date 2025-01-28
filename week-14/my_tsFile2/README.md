7187b2de84fb2c73637c7f9b5fc47809c5a3b422
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2025-01-28 22:10:20 +0530

    make one key of interface optional in typescript

--- week-14/my_tsFile2/README.md
+++ week-14/my_tsFile2/README.md
@@ -1,1 +1,15 @@
 ## Start Typescript 
+ npm init -y
+
+npm install -g typescript 
+
+
+tsc
+
+npx tsc -b
+
+
+node index.js
+
+
+node dist/index.js
--- week-14/my_tsFile2/src/optionalKeyInter.ts
+++ week-14/my_tsFile2/src/optionalKeyInter.ts
@@ -1,28 +1,33 @@
 // complex object we can defined by interface 
-interface User {
+interface User1 {
     name: string;
     age: number;
-    address : {
+    address?: {
         city: string;
         country: string;
         pincode : number;
     };
 }
+//to make anything optional we have to just add "?"
+let user2: User1 ={
+    name:"puja",
+    age: 16,
+}
 
 //this user has  type User
-let user: User ={
+let user1: User1 ={
     name:"puja",
     age: 22,
     address: {
         city:"kolkata",
         country: "india",
         pincode :700044
-    }
+    },
 }
 
 //this take user as input and return a boolean
-function isLegal (user: User): boolean {
-    if(user.age >= 18){
+function isLegal1 (user2: User1): boolean {
+    if(user2.age >= 18){
         return true 
     }else {
         return false
@@ -31,8 +36,8 @@ function isLegal (user: User): boolean {
 
 
 //call that function with the user 
-const ans=isLegal(user);
-if(ans){
+const ans1=isLegal1(user2);
+if(ans1){
     console.log("I am legal");
 }else {
     console.log("I am illegal")
